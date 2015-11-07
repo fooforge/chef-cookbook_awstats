42948dc1a94b5b6365424be43b08821ec89b0f59
xcaliburinhand <github@muteheadlight.net>
2015-11-07 13:33:52 -0500

    chef 12.x requires name attribute in metadata

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,8 @@ maintainer_email "mike@fooforge.com"
 license          "Apache 2.0"
 description      "Installs awstats and provides an LWRP for creating domain-specific statistics"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.2"
+version          "0.2.3"
+name             "awstats"
 
 depends          "apache2"
 depends          "htpasswd"
